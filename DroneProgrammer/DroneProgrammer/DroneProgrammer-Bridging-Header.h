c49f3c0f98e743dc3d88fad62cf45a7f60231ada
mathiasTonini <43469697+mathiasTonini@users.noreply.github.com>
2020-03-12 15:59:31 +0100

    clean project

--- DroneProgrammer/DroneProgrammer/DroneProgrammer-Bridging-Header.h
+++ DroneProgrammer/DroneProgrammer/DroneProgrammer-Bridging-Header.h
@@ -3,13 +3,13 @@
 //
 
 //objc headers
-#import "../../SDKSample/SDKSample/Views/H264VideoView.h"
-#import "../../SDKSample/SDKSample/Audio/AudioStreamAUBackend.h"
-#import "../../SDKSample/SDKSample/Drones/BebopDrone.h"
-#import "../../SDKSample/SDKSample/Drones/SDCardModule.h"
-#import "../../SDKSample/SDKSample/ViewControllers/BebopVC.h"
-#import "../../SDKSample/SDKSample/ViewControllers/DeviceListVC.h"
-#import "../../SDKSample/SDKSample/Discovery/DroneDiscoverer.h"
+#import "../../Externe/Samples/iOS/SDKSample/SDKSample/Views/H264VideoView.h"
+#import "../../Externe/Samples/iOS/SDKSample/SDKSample/Audio/AudioStreamAUBackend.h"
+#import "../../Externe/Samples/iOS/SDKSample/SDKSample/Drones/BebopDrone.h"
+#import "../../Externe/Samples/iOS/SDKSample/SDKSample/Drones/SDCardModule.h"
+#import "../../Externe/Samples/iOS/SDKSample/SDKSample/ViewControllers/BebopVC.h"
+#import "../../Externe/Samples/iOS/SDKSample/SDKSample/ViewControllers/DeviceListVC.h"
+#import "../../Externe/Samples/iOS/SDKSample/SDKSample/Discovery/DroneDiscoverer.h"
 #import <libARDiscovery/ARDISCOVERY_BonjourDiscovery.h>
 
 #import "DeviceListVC2.h"
