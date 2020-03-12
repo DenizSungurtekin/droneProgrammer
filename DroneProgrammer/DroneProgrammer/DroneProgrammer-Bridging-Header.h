e25315640233ccdbf71f7f95e7e709c7d2aaf3af
mathiasTonini <43469697+mathiasTonini@users.noreply.github.com>
2020-03-12 15:33:56 +0100

    add project

---
+++ DroneProgrammer/DroneProgrammer/DroneProgrammer-Bridging-Header.h
@@ -0,0 +1,18 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+//objc headers
+#import "../../SDKSample/SDKSample/Views/H264VideoView.h"
+#import "../../SDKSample/SDKSample/Audio/AudioStreamAUBackend.h"
+#import "../../SDKSample/SDKSample/Drones/BebopDrone.h"
+#import "../../SDKSample/SDKSample/Drones/SDCardModule.h"
+#import "../../SDKSample/SDKSample/ViewControllers/BebopVC.h"
+#import "../../SDKSample/SDKSample/ViewControllers/DeviceListVC.h"
+#import "../../SDKSample/SDKSample/Discovery/DroneDiscoverer.h"
+#import <libARDiscovery/ARDISCOVERY_BonjourDiscovery.h>
+
+#import "DeviceListVC2.h"
+
+//swift headers
+//import "ViewController.h"
