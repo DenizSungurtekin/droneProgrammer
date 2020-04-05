fee00b019067998a0c35e4ba7c81dcf0203efea3
mathiasTonini <43469697+mathiasTonini@users.noreply.github.com>
2020-04-05 17:11:42 +0200

    add cube simulation

--- DroneProgrammer/DroneProgrammer/SimulationView.swift
+++ DroneProgrammer/DroneProgrammer/SimulationView.swift
@@ -23,14 +23,33 @@ class SimulationView: UIViewController ,UIAlertViewDelegate{
         super.viewDidLoad()
         commandes = tmpCmd;
         obstacles = tmpObs;
-        for el in obstacles {
-            print (el.write())
-        }
-        print(commandes)
+        prepareScence()
         
-
     }
-   
+    func prepareScence(){
+        let sceneView = SCNView(frame: self.view.frame)
+        self.view.addSubview(sceneView)
+        sceneView.autoenablesDefaultLighting = true
+        sceneView.allowsCameraControl = true
+            
+        let scene = SCNScene()
+        sceneView.scene = scene
+
+        let camera = SCNCamera()
+        let cameraNode = SCNNode()
+        cameraNode.camera = camera
+        cameraNode.position = SCNVector3(x: -3.0, y: 3.0, z: 1.0)
+
+        let cubeGeometry = SCNBox(width: 3.0, height: 3.0, length: 3.0, chamferRadius: 0.0)
+        let cubeNode = SCNNode(geometry: cubeGeometry)
+        cubeGeometry.firstMaterial?.diffuse.contents = UIColor.lightGray
+
+        scene.rootNode.addChildNode(cameraNode)
+        scene.rootNode.addChildNode(cubeNode)
+        let constraint = SCNLookAtConstraint(target: cubeNode)
+        constraint.isGimbalLockEnabled = true
+        cameraNode.constraints = [constraint]
+    }
 
     
     
